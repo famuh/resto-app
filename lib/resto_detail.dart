b228214962502b591c4ab6898b542fb7cff70556
Fadhil Muhammad <famuh79@gmail.com>
2022-08-29 21:38:14 +0700

    Details page done

--- lib/resto_detail.dart
+++ lib/resto_detail.dart
@@ -16,7 +16,7 @@ class RestoDetails extends StatelessWidget {
           SliverAppBar(
             elevation: 0,
             shadowColor: Colors.transparent,
-            expandedHeight: 250,
+            expandedHeight: 300,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.network(restaurant.pictureId, fit: BoxFit.fill),
             ),
@@ -24,81 +24,86 @@ class RestoDetails extends StatelessWidget {
         ];
       },
       body: SingleChildScrollView(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Icon(Icons.star_rounded, color: Colors.amber),
-                Text(restaurant.rating.toString())
-              ],
-            ),
-            ListTile(
-              title: Text(restaurant.name),
-              subtitle: Text(restaurant.city),
-            ),
-
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                restaurant.description,
-                textAlign: TextAlign.justify,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Icon(Icons.star_rounded, color: Colors.amber),
+                  Text(
+                    restaurant.rating.toString(),
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
               ),
-            ),
+              ListTile(
+                leading: const Icon(Icons.food_bank_rounded, size: 50),
+                title: Text(restaurant.name,
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
+                subtitle: Text(restaurant.city),
+              ),
 
-            // foods
-            const Text('Foods'),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 60,
-              child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: restaurant.menus.foods.length,
-                itemBuilder: (context, index) => Center(
-                  child: Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.green),
-                          borderRadius: BorderRadius.circular(10)),
-                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                      child: Text(restaurant.menus.foods[index].name)),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  restaurant.description,
+                  textAlign: TextAlign.justify,
+                  style: const TextStyle(color: Colors.black54),
                 ),
               ),
-            ),
 
-            //drinks
-            const Text('Drinks'),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 60,
-              child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: restaurant.menus.foods.length,
-                itemBuilder: (context, index) => Center(
-                  child: Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.green),
-                          borderRadius: BorderRadius.circular(10)),
-                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                      child: Text(restaurant.menus.drinks[index].name)),
+              // foods
+              const Text('Foods', style: TextStyle(fontWeight: FontWeight.w500)),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: restaurant.menus.foods.length,
+                  itemBuilder: (context, index) => Center(
+                    child: Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 4),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.green),
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 4),
+                        child: Text(restaurant.menus.foods[index].name)),
+                  ),
                 ),
               ),
-            )
-          ],
+
+              //drinks
+              const Text('Drinks', style: TextStyle(fontWeight: FontWeight.w500)),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: restaurant.menus.foods.length,
+                  itemBuilder: (context, index) => Center(
+                    child: Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 4),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.green),
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 4),
+                        child: Text(restaurant.menus.drinks[index].name)),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     ));
   }
 }
-
-// Widget _buildFoodItem(BuildContext context, Restaurant resto){
-//   return Card(
-//     child: Text(resto.menus.drinks)
-//   );
-// }
