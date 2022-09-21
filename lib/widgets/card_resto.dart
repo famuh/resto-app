1490baf16bf08988a7e1fcdbf63ad9e4274e1170
Fadhil Muhammad <famuh79@gmail.com>
2022-09-22 00:26:44 +0700

    memperbaiki overflow dan pembacaan data pada card resto widget

--- lib/widgets/card_resto.dart
+++ lib/widgets/card_resto.dart
@@ -1,54 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:resto_app/data/model/restaurant.dart';
 import 'package:resto_app/data/model/search_restaurant.dart';
-import 'package:resto_app/resto_detail.dart';
+import 'package:resto_app/provider/database_provider.dart';
+
+import '../screen/resto_detail.dart';
 
 // UNTUK HOMEPAGE
-class CardRestaurant extends StatelessWidget {
+class CardRestaurant extends StatefulWidget {
   final Restaurant? restaurant;
   const CardRestaurant({Key? key, this.restaurant}) : super(key: key);
 
   @override
+  State<CardRestaurant> createState() => _CardRestaurantState();
+}
+
+class _CardRestaurantState extends State<CardRestaurant> {
+  bool? _isFavorite;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _isFavorite = false;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      child: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                opacity: .2,
-                image: NetworkImage(
-                    'https://restaurant-api.dicoding.dev/images/small/${restaurant?.pictureId}'))),
-        child: ListTile(
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(restaurant!.name,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w500, fontSize: 16)),
-              SizedBox(
-                width: MediaQuery.of(context).size.width / 3,
-                child: Row(
-                  children: [
-                    const Icon(
-                      Icons.location_on_rounded,
-                      size: 16,
-                      color: Colors.redAccent,
-                    ),
-                    Text(' ' + restaurant!.city)
-                  ],
+    return Consumer<DatabaseProvider>(
+      builder: (context, provider, child) {
+        return FutureBuilder(
+          future: provider.isFavorited(widget.restaurant!.id),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            var isFavorited = snapshot.data ?? false;
+            return Card(
+              elevation: 2,
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        opacity: .2,
+                        image: NetworkImage(
+                            'https://restaurant-api.dicoding.dev/images/small/${widget.restaurant?.pictureId}'))),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(widget.restaurant!.name,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 16)),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 3.5,
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.location_on_rounded,
+                              size: 16,
+                              color: Colors.redAccent,
+                            ),
+                            Text(widget.restaurant!.city)
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: isFavorited
+                      ? IconButton(
+                          onPressed: () =>
+                              provider.removeFavorite(widget.restaurant!.id),
+                          icon: const Icon(Icons.favorite_rounded),
+                          color: Colors.pinkAccent,
+                        )
+                      : IconButton(
+                          onPressed: () =>
+                              provider.addFavorite(widget.restaurant!),
+                          icon: const Icon(Icons.favorite_outline_rounded),
+                          color: Colors.pinkAccent,
+                        ),
+                  onTap: () {
+                    Navigator.pushNamed(context, RestoDetails.routeName,
+                        arguments: widget.restaurant!.id);
+                  },
                 ),
-              )
-            ],
-          ),
-          onTap: () {
-            Navigator.pushNamed(context, RestoDetails.routeName,
-                arguments: restaurant!.id);
+              ),
+            );
           },
-        ),
-      ),
+        );
+      },
     );
   }
 }
@@ -62,45 +102,68 @@ class CardRestaurantSearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      child: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                opacity: .2,
-                image: NetworkImage(
-                    'https://restaurant-api.dicoding.dev/images/small/${restaurant.pictureId}'))),
-        child: ListTile(
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(restaurant.name,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w500, fontSize: 16)),
-              SizedBox(
-                width: MediaQuery.of(context).size.width / 3,
-                child: Row(
-                  children: [
-                    const Icon(
-                      Icons.location_on_rounded,
-                      size: 16,
-                      color: Colors.redAccent,
-                    ),
-                    Text(' ' + restaurant.city)
-                  ],
+    return Consumer<DatabaseProvider>(
+      builder: (context, provider, child) {
+        return FutureBuilder(
+          future: provider.isFavorited(restaurant.id),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            var isFavorited = snapshot.data ?? false;
+            return Card(
+              elevation: 2,
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        opacity: .2,
+                        image: NetworkImage(
+                            'https://restaurant-api.dicoding.dev/images/small/${restaurant.pictureId}'))),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(restaurant.name,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 16)),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 3,
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.location_on_rounded,
+                              size: 16,
+                              color: Colors.redAccent,
+                            ),
+                            Text(restaurant.city)
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: isFavorited
+                      ? IconButton(
+                          onPressed: () =>
+                              provider.removeFavorite(restaurant.id),
+                          icon: const Icon(Icons.favorite_rounded),
+                          color: Colors.pinkAccent,
+                        )
+                      : IconButton(
+                          onPressed: () =>
+                              provider.addFavoriteSearch(restaurant),
+                          icon: const Icon(Icons.favorite_outline_rounded),
+                          color: Colors.pinkAccent,
+                        ),
+                  onTap: () {
+                    Navigator.pushNamed(context, RestoDetails.routeName,
+                        arguments: restaurant.id);
+                  },
                 ),
-              )
-            ],
-          ),
-          onTap: () {
-            Navigator.pushNamed(context, RestoDetails.routeName,
-                arguments: restaurant.id);
+              ),
+            );
           },
-        ),
-      ),
+        );
+      },
     );
   }
 }
